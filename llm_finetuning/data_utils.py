a10faee31f80f8ccf9f13e57fe62a1330fd5c734
Siddharth Singh <siddharth9820@gmail.com>
2024-06-09 21:26:31 -0700

    save model at the end

--- llm_finetuning/data_utils.py
+++ llm_finetuning/data_utils.py
@@ -74,20 +74,20 @@ def tokenize(prompt, tokenizer, cutoff_len, add_eos_token=True):
         return result
 
 
-def get_tokenizer_mapping_fn(tokenizer, cutoff_len, train_on_inputs=True):
+def get_tokenizer_mapping_fn(tokenizer, cutoff_len, train_on_inputs=True, add_eos_token=True):
     def generate_and_tokenize_prompt(data_point):
         full_prompt = prompter.generate_prompt(
             instruction = data_point["instruction"],
-            context = data_point["context"],
-            response = data_point["response"],
+            context = data_point["input"],
+            response = data_point["output"],
         )
-        tokenized_full_prompt = tokenize(full_prompt, tokenizer, cutoff_len)
+        tokenized_full_prompt = tokenize(full_prompt, tokenizer, cutoff_len, add_eos_token)
         if not train_on_inputs:
             user_prompt = prompter.generate_prompt(
                 data_point["instruction"], data_point["input"]
             )
             tokenized_user_prompt = tokenize(
-                user_prompt, add_eos_token=add_eos_token
+                user_prompt, tokenizer,  cutoff_len, add_eos_token
             )
             user_prompt_len = len(tokenized_user_prompt["input_ids"])
 
--- llm_finetuning/finetune.py
+++ llm_finetuning/finetune.py
@@ -10,6 +10,7 @@ from argparse import ArgumentParser
 from contextlib import nullcontext
 from data_utils import get_tokenizer_mapping_fn
 from torch.utils.data import DataLoader
+from axonn.checkpoint import save
 
 def init_everything():
     torch.distributed.init_process_group(backend='nccl')
@@ -20,7 +21,6 @@ def init_everything():
     ax.init(G_data=1, G_inter=1, G_intra_r=world_size, G_intra_c=1, G_intra_d=1)
 
 def set_seed(seed=123456):
-    # Extremely important for AxoNN
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -42,12 +42,14 @@ def create_parser():
                         help="Gradient Accumulation Steps")
     parser.add_argument("--sequence-length", type=int, default=256, 
                         help="Sequence Length")
-    parser.add_argument("--num-iters", type=int, default=1000, 
-                        help="Number of Training Iterations")
     parser.add_argument("--disable-axonn", action='store_false', dest='use_axonn',
                         help="Disable AxoNN's Tensor Paralellism")
     parser.add_argument("--log-interval", type=int, default=10,
                         help="Interval for logging train loss")
+    parser.add_argument("--num-epochs", type=int, default=3,
+                        help="Number of epochs")
+    parser.add_argument("--save-every", type=int, default=100,
+                        help="Save model weights after every --save-every iterations")
     return parser
 
 dtype_map = {
@@ -58,8 +60,8 @@ dtype_map = {
 
 
 def get_tokenized_dataset(tokenizer, sequence_length=256):
-    data = load_dataset("databricks/databricks-dolly-15k")
-    mapping_fn = get_tokenizer_mapping_fn(tokenizer, cutoff_len=sequence_length)
+    data = load_dataset("tatsu-lab/alpaca")
+    mapping_fn = get_tokenizer_mapping_fn(tokenizer, cutoff_len=sequence_length, train_on_inputs=False)
     train_data = data["train"].shuffle().map(mapping_fn, remove_columns=data["train"].column_names)
     return train_data
 
@@ -113,7 +115,21 @@ if __name__ == "__main__":
                                 collate_fn=DataCollatorForSeq2Seq(
                                         tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True)
                             ) 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5, weight_decay=0)
+    optimizer = torch.optim.AdamW(model.parameters(), 
+                                  lr=1e-5, 
+                                  betas=(0.9, 0.95), 
+                                  eps=1e-5,
+                                  weight_decay=0.0)
+
+    iters_per_epoch = len(dataloader) // args.gradient_acc_steps
+    main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=iters_per_epoch * args.num_epochs)
+    warmup_iters = 100
+    warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
+                optimizer, start_factor=0.01, total_iters=warmup_iters
+            )
+    lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
+            optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[warmup_iters]
+        )
 
     scaler = torch.cuda.amp.GradScaler(enabled=(dtype == torch.float16))
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -121,12 +137,13 @@ if __name__ == "__main__":
     start_event = torch.cuda.Event(enable_timing=True)
     end_event = torch.cuda.Event(enable_timing=True)
     iter_no = 0
-    for batch in dataloader:
+    for epoch_no in range(args.num_epochs):
+        microbatch_no = 0
         start_event.record()
         batch_loss = 0
-        input_ids, labels, attention_mask = batch["input_ids"], batch["labels"], batch["attention_mask"]
-        input_ids, labels, attention_mask = input_ids.cuda(), labels.cuda(), attention_mask.cuda()
-        for grad_acc_step in range(args.gradient_acc_steps):
+        for batch in dataloader:
+            input_ids, labels, attention_mask = batch["input_ids"], batch["labels"], batch["attention_mask"]
+            input_ids, labels, attention_mask = input_ids.cuda(), labels.cuda(), attention_mask.cuda()
             with torch.amp.autocast(device_type='cuda', dtype=dtype):
                 input_ids = input_ids[:, :-1]
                 attention_mask = attention_mask[:, :-1]
@@ -137,18 +154,34 @@ if __name__ == "__main__":
                                labels.reshape(-1))
             scaler.scale(loss / args.gradient_acc_steps).backward()
             batch_loss += loss / args.gradient_acc_steps
-        scaler.unscale_(optimizer)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        scaler.step(optimizer)
-        scaler.update()
-        optimizer.zero_grad(set_to_none=True)
-        iter_no += 1
-        end_event.record()
-        if torch.distributed.get_rank() == 0 and (iter_no % args.log_interval==0):
-            torch.cuda.synchronize()
-            elapsed_time = start_event.elapsed_time(end_event) 
-            log_string = pretty_log(iter_no, args.num_iters, batch_loss.item(), elapsed_time, learning_rate=optimizer.param_groups[0]['lr'])
-            print(log_string)
+            microbatch_no += 1
+
+            if microbatch_no == args.gradient_acc_steps:
+                scaler.unscale_(optimizer)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad(set_to_none=True)
+                lr_scheduler.step()
+                iter_no += 1
+                end_event.record()
+                if torch.distributed.get_rank() == 0 and (iter_no % args.log_interval==0):
+                    torch.cuda.synchronize()
+                    elapsed_time = start_event.elapsed_time(end_event) 
+                    log_string = pretty_log(iter_no, len(dataloader)*args.num_epochs // args.gradient_acc_steps, 
+                                            batch_loss.item(), elapsed_time, learning_rate=optimizer.param_groups[0]['lr'])
+                    print(log_string)
+
+                microbatch_no = 0
+                batch_loss = 0
+                start_event.record()
+                state = {
+                        "iter_no": iter_no,
+                        "optimizer": optimizer.state_dict(),
+                        "model": model.state_dict()
+                        }
+                
+    save(state, checkpoint_folder="./ckpt", checkpoint_name=f"iter_{iter_no}")
 
  
 
