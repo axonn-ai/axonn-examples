a45d1d7a372c6b24bcda9ed335610e5e5c4b5571
Siddharth Singh <siddharth9820@gmail.com>
2024-07-01 12:22:50 -0400

    Creating a Pytorch Lightning finetuning example (#6)

--- llm_finetuning/finetune_pl.py
+++ llm_finetuning/finetune_pl.py
@@ -6,32 +6,64 @@ from transformers import (
     DataCollatorForSeq2Seq,
 )
 from datasets import load_dataset
-from axonn.models.transformers import parallelize
-from axonn import axonn as ax
 import torch
+
+# Initializing mpi4py is necessary to use
+# pytorch lightning within interactive slurm
+# sesions
+try:
+    from mpi4py import MPI
+except ImportError:
+    pass
+
+from axonn import axonn as ax
 import random
 import numpy as np
 from argparse import ArgumentParser
 from contextlib import nullcontext
 from data_utils import get_tokenizer_mapping_fn
 from torch.utils.data import DataLoader
-from axonn.checkpoint import save
+
+from lightning.fabric import Fabric, seed_everything
+
+from axonn.lightning import AxonnStrategy
+from lightning.pytorch.strategies import DeepSpeedStrategy
+from lightning.fabric.strategies import FSDPStrategy
 
 
-def init_everything():
+def init_everything(dtype, num_nodes, strategy="axonn"):
     torch.distributed.init_process_group(backend="nccl")
     world_size = torch.distributed.get_world_size()
-    rank = torch.distributed.get_rank()
-    if rank == 0:
+    if strategy == "axonn":
+        pl_strategy = AxonnStrategy(G_intra_d=world_size)
+    elif strategy == "fsdp":
+        from transformers.models.llama.modeling_llama import LlamaDecoderLayer as Block
+        pl_strategy = FSDPStrategy(
+            auto_wrap_policy={Block},
+            activation_checkpointing_policy={Block},
+            state_dict_type="full",
+            limit_all_gathers=True,
+            cpu_offload=False,
+        )
+    elif strategy == "deepspeed":
+        pl_strategy = "deepspeed_stage_3"
+
+    fabric = Fabric(
+        strategy=pl_strategy,
+        devices=torch.cuda.device_count(),
+        num_nodes=num_nodes,
+        precision=dtype,
+    )
+    fabric.launch()
+
+    if torch.distributed.get_rank() == 0:
         print(f"Going to distribute the model over {world_size} GPUs")
-    ax.init(G_data=1, G_inter=1, G_intra_r=world_size, G_intra_c=1, G_intra_d=1)
+
+    return fabric
 
 
 def set_seed(seed=123456):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    seed_everything(seed)
 
 
 def create_parser():
@@ -45,9 +77,15 @@ def create_parser():
     parser.add_argument("--seed", type=int, default=123456, help="random seed")
     parser.add_argument(
         "--dtype",
-        choices=["bf16", "fp16", "fp32"],
-        help="data type for running inference",
-        default="fp16",
+        choices=["bf16-mixed", "16-mixed", "32"],
+        help="data type for finetuning",
+        default="bf16-mixed",
+    )
+    parser.add_argument(
+        "--strategy",
+        choices=["axonn", "fsdp", "deepspeed"],
+        help="Choose Parallel backend to use",
+        default="axonn",
     )
     parser.add_argument(
         "--use-flash-attention",
@@ -64,25 +102,29 @@ def create_parser():
         "--sequence-length", type=int, default=256, help="Sequence Length"
     )
     parser.add_argument(
-        "--disable-axonn",
-        action="store_false",
-        dest="use_axonn",
-        help="Disable AxoNN's Tensor Paralellism",
+        "--num-nodes",
+        type=int,
+        default=1,
+        help="Number of nodes (this needs to be passed explicitly for lightning fabric)",
     )
     parser.add_argument(
         "--log-interval", type=int, default=10, help="Interval for logging train loss"
     )
     parser.add_argument("--num-epochs", type=int, default=3, help="Number of epochs")
     parser.add_argument(
-        "--save-every",
-        type=int,
-        default=100,
-        help="Save model weights after every --save-every iterations",
+        "--wandb-log", action="store_true", help="Use Wandb for logging"
+    )
+    parser.add_argument(
+        "--wandb-project",
+        type=str,
+        help="Name of wandb project (only necessary when --wandb-log is used)",
+    )
+    parser.add_argument(
+        "--wandb-run-name",
+        type=str,
+        help="Name of wandb run (only necessary when --wandb-log is used)",
     )
     return parser
-
-
-dtype_map = {"bf16": torch.bfloat16, "fp16": torch.float16, "fp32": torch.float32}
 
 
 def get_tokenized_dataset(tokenizer, sequence_length=256):
@@ -104,6 +146,7 @@ def pretty_log(
     train_loss,
     elapsed_time_per_iteration,
     learning_rate,
+    wandb_log=False,
 ):
 
     log_string = "> global batch {:8d}/{:8d} |".format(iteration, total_train_iters)
@@ -117,58 +160,72 @@ def pretty_log(
     log_string += " memory used by tensors {:.3f} GB (peak {:.3f} GB) ".format(
         curr_mem, peak_mem
     )
+    if wandb_log:
+        import wandb
+        wandb.log(
+            {
+                "iter": iteration,
+                "train/loss": train_loss,
+                "lr": learning_rate,
+                "peak_memory": peak_mem,
+                "state_memory": curr_mem,
+                "iteration_time": elapsed_time_per_iteration,
+            }
+        )
     return log_string
 
+
+dtype_map = {
+    "bf16-mixed": torch.bfloat16,
+    "16-mixed": torch.float16,
+    "32": torch.float32,
+}
 
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
-    init_everything()
+    fabric = init_everything(args.dtype, args.num_nodes, args.strategy)
     set_seed(args.seed)
-    dtype = dtype_map[args.dtype]
+    if args.wandb_log and torch.distributed.get_rank() == 0:
+        import wandb
 
-    if args.use_axonn:
-        with parallelize(args.model_id):
-            model = (
-                AutoModelForCausalLM.from_pretrained(
-                    args.model_id,
-                    torch_dtype=dtype,
-                    attn_implementation=(
-                        "eager" if not args.use_flash_attention else "flash_attention_2"
-                    ),
-                )
-                .to("cuda")
-                .float()
-            )
-    else:
-        model = (
-            AutoModelForCausalLM.from_pretrained(
-                args.model_id,
-                torch_dtype=dtype,
-                attn_implementation=(
-                    "eager" if not args.use_flash_attention else "flash_attention_2"
-                ),
-            )
-            .to("cuda")
-            .float()
-        )
+        wandb.init(project=args.wandb_project, name=args.wandb_run_name, config=args)
+
+    with fabric.init_module():
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_id,
+            attn_implementation=(
+                "eager" if not args.use_flash_attention else "flash_attention_2"
+            ),
+        ).to("cuda")
 
     model.train()
-    model.gradient_checkpointing_enable()
+    if args.strategy != "fsdp":
+        model.gradient_checkpointing_enable()
+    
+    model = fabric.setup_module(model)
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=1e-5, betas=(0.9, 0.95), eps=1e-5, weight_decay=0.0
+    )
+
+    optimizer = fabric.setup_optimizers(optimizer)
+
     tokenizer = AutoTokenizer.from_pretrained(args.model_id)
     tokenizer.pad_token_id = 0
     tokenizer.padding_side = "left"
     tokenized_dataset = get_tokenized_dataset(tokenizer, args.sequence_length)
     dataloader = DataLoader(
         tokenized_dataset,
-        batch_size=args.global_batch_size // args.gradient_acc_steps,
+        batch_size=args.global_batch_size
+        // args.gradient_acc_steps
+        // torch.distributed.get_world_size(),
         collate_fn=DataCollatorForSeq2Seq(
             tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
         ),
     )
-    optimizer = torch.optim.AdamW(
-        model.parameters(), lr=1e-5, betas=(0.9, 0.95), eps=1e-5, weight_decay=0.0
-    )
+
+    dataloader = fabric.setup_dataloaders(dataloader)
 
     iters_per_epoch = len(dataloader) // args.gradient_acc_steps
     main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
@@ -184,7 +241,6 @@ if __name__ == "__main__":
         milestones=[warmup_iters],
     )
 
-    scaler = torch.cuda.amp.GradScaler(enabled=(dtype == torch.float16))
     loss_fn = torch.nn.CrossEntropyLoss()
 
     start_event = torch.cuda.Event(enable_timing=True)
@@ -205,22 +261,25 @@ if __name__ == "__main__":
                 labels.cuda(),
                 attention_mask.cuda(),
             )
-            with torch.amp.autocast(device_type="cuda", dtype=dtype):
-                input_ids = input_ids[:, :-1]
-                attention_mask = attention_mask[:, :-1]
-                labels = labels[:, 1:]
+            input_ids = input_ids[:, :-1]
+            attention_mask = attention_mask[:, :-1]
+            labels = labels[:, 1:]
+            ctx = (
+                fabric._strategy.optimize_communication(model)
+                if args.strategy == "axonn"
+                else nullcontext()
+            )
+            with ctx:
                 output = model(input_ids=input_ids, attention_mask=attention_mask)
                 logits = output["logits"]
                 loss = loss_fn(logits.reshape(-1, logits.shape[-1]), labels.reshape(-1))
-            scaler.scale(loss / args.gradient_acc_steps).backward()
-            batch_loss += loss / args.gradient_acc_steps
+                fabric.backward(loss / args.gradient_acc_steps, model=model)
+            batch_loss += (loss / args.gradient_acc_steps).item()
             microbatch_no += 1
 
             if microbatch_no == args.gradient_acc_steps:
-                scaler.unscale_(optimizer)
-                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-                scaler.step(optimizer)
-                scaler.update()
+                fabric.clip_gradients(model, optimizer, 1.0)
+                optimizer.step()
                 optimizer.zero_grad(set_to_none=True)
                 lr_scheduler.step()
                 iter_no += 1
@@ -233,19 +292,18 @@ if __name__ == "__main__":
                     log_string = pretty_log(
                         iter_no,
                         len(dataloader) * args.num_epochs // args.gradient_acc_steps,
-                        batch_loss.item(),
+                        batch_loss,
                         elapsed_time,
                         learning_rate=optimizer.param_groups[0]["lr"],
+                        wandb_log=args.wandb_log,
                     )
                     print(log_string)
 
                 microbatch_no = 0
                 batch_loss = 0
                 start_event.record()
-                state = {
-                    "iter_no": iter_no,
-                    "optimizer": optimizer.state_dict(),
-                    "model": model.state_dict(),
-                }
 
-    save(state, checkpoint_folder="./ckpt", checkpoint_name=f"iter_{iter_no}")
+        state = {"optimizer": optimizer.state_dict(), "model": model.state_dict()}
+
+        fabric.save(f"./ckpt/ckpt_model_{epoch_no}.pt", state["model"])
+        fabric.save(f"./ckpt/ckpt_optimizer_{epoch_no}.pt", state["optimizer"])
